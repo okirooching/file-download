d658d50a30547e03646ca591e8a7d4e588af8d61
okirooching <okiroochienganywhere@gmail.com>
2025-08-08 00:02:00 +0300

    created a view to redirect to pdf file

--- pdfserver/pdfserver/urls.py
+++ pdfserver/pdfserver/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('receipt/', include('receipt.urls')),
+    path('', include('receipt.urls')),
 ]
--- pdfserver/receipt/views.py
+++ pdfserver/receipt/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+def index(request):
+    return redirect('https://tinyurl.com/TEVYE8JL-07-08-2025')
